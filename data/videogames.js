a4b3ae1dda288cae97e54c9733112ecf8a484cc6
Dylan-Greathouse <Greathouse.dylanj@gmail.com>
2021-08-18 11:33:50 -0700

    added images to videogame data, changed file formate from assests to the URL of image

--- data/videogames.js
+++ data/videogames.js
@@ -4,7 +4,7 @@ module.exports = [
     system_id: 1,
     played: true,
     year_released: 2009,
-    image_url: './assets/demons-souls-ps3.jpg',
+    image_url: 'https://images.pushsquare.com/7c0bd4ec9821f/demons-souls-ps3-playstation-3.original.jpg',
   },
 
   {
@@ -12,7 +12,7 @@ module.exports = [
     system_id: 1,
     played: true,
     year_released: 2011,
-    image_url: './assets/dark-souls.jpg',
+    image_url: 'https://i.pcmag.com/imagery/lineups/002J1z9vQPxQWnT8X9LziZD-1.1569492958.fit_lim.size_1200x630.jpg',
   },
 
   {
@@ -20,7 +20,7 @@ module.exports = [
     system_id: 1,
     played: true,
     year_released: 2014,
-    image_url: './assets/dark-soul2.jpg',
+    image_url: 'https://cdn.igromania.ru/mnt/articles/0/9/6/d/f/4/26444/html/img/77b44acf46db0c81.jpg',
   },
 
   {
@@ -28,7 +28,7 @@ module.exports = [
     system_id: 2,
     played: false,
     year_released: 2015,
-    image_url: './assets/bloodborne.jpg',
+    image_url: 'https://media.npr.org/assets/img/2020/04/07/bb_tgs_6_wide-018769fbfe88268abddef71c79380be087068c81.jpg',
   },
 
   {
@@ -36,7 +36,7 @@ module.exports = [
     system_id: 1,
     played: true,
     year_released: 2016,
-    image_url: './assets/dark-souls3.jpg',
+    image_url: 'https://media.wired.com/photos/5a3d54832db63c66d811174a/master/pass/ds3.jpg',
   },
 
   {
@@ -44,7 +44,7 @@ module.exports = [
     system_id: 2,
     played: true,
     year_released: 2019,
-    image_url: 'assets/sekiro.jpg',
+    image_url: 'https://img.redbull.com/images/c_crop,w_1350,h_900,x_0,y_0,f_auto,q_auto/c_scale,w_1500/redbullcom/2019/04/01/6c7e5632-5ee2-496f-81fd-f526f04ddcf5/sekiro-grappling-hook',
   },
 
   {
